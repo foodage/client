6ed8b199425714c1d523d84682ff1056a0d54f7d
Subin Lee <ewaterbinn@gmail.com>
2024-01-04 16:16:31 +0900

    fix: lint 적용

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,8 +3,8 @@ export default function Head() {
     <>
       <title>FOODAGE | Meal Diary</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Foodage, Meal Diary" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta content="Foodage, Meal Diary" name="description" />
+      <link href="/favicon.ico" rel="icon" />
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from 'next';
 import '@/styles/globals.scss';
+
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '푸디지 | Meal Diary',
