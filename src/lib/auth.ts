1d75ff0977d968910842575300f55996338bbd27
Subin Lee <ewaterbinn@gmail.com>
2024-01-30 20:38:21 +0900

    feat: next auth 작성 및 적용

---
+++ src/lib/auth.ts
@@ -0,0 +1,75 @@
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import Kakao from 'next-auth/providers/kakao';
+
+/**
+ * api/auth/[...next auth]/route.ts 에서 사용
+ *
+ * [Note]
+ * - kakao login 🏂
+ * - naver login
+ * - google login
+ *
+ * 임의 작성
+ */
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: '/signin',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {},
+
+      async authorize(credentials) {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+
+        if (!username || !password) {
+          return null;
+        }
+
+        try {
+          const res = {
+            id: 'id',
+            token: 'token',
+          };
+
+          return {
+            id: res.id,
+            token: res.token,
+          };
+        } catch (err) {
+          console.log(err);
+
+          return null;
+        }
+      },
+    }),
+    Kakao({
+      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY!,
+      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      user && (token.user = user);
+      return Promise.resolve(token);
+    },
+    session: async ({ session, token }) => {
+      /*@ts-ignore */
+      session = token.user;
+      return Promise.resolve(session);
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  session: {
+    strategy: 'jwt',
+  },
+};
