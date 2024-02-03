096d8042d1dea0c019d598a0575d4da1f4a3eeda
Sam <wtawonlife@gmail.com>
2024-02-03 16:06:15 +0900

    feat: naver/kakao sdk setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,16 +6,19 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { Providers } from '@/utils';
 
-export const metadata: Metadata = {
-  title: '푸디지 | Meal Diary',
-  description: 'Meal Diary',
-};
+import Head from './head';
+
+// export const metadata: Metadata = {
+//   title: '푸디지 | Meal Diary',
+//   description: 'Meal Diary',
+// };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
   return (
     <html lang="ko">
+      <Head />
       <Providers session={session}>
         <body>{children}</body>
       </Providers>
