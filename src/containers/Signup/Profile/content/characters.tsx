bb88c7acc68c1b36ff78822dcd1aa537dfdc6210
Subin Lee <ewaterbinn@gmail.com>
2024-02-13 01:01:26 +0900

    add: 프로필 이미지 content 작성

---
+++ src/containers/Signup/Profile/content/characters.tsx
@@ -0,0 +1,36 @@
+const characters = [
+  {
+    value: 'Pigeon',
+    image: '/assets/characters/비둘기.svg',
+  },
+  {
+    value: 'Sparrow',
+    image: '/assets/characters/참새.svg',
+  },
+  {
+    value: 'Chick',
+    image: '/assets/characters/병아리.svg',
+  },
+  {
+    value: 'Squirrel',
+    image: '/assets/characters/다람쥐.svg',
+  },
+  {
+    value: 'Racon',
+    image: '/assets/characters/너구리.svg',
+  },
+  {
+    value: 'Seal',
+    image: '/assets/characters/물개.svg',
+  },
+  {
+    value: 'Badger',
+    image: '/assets/characters/오소리.svg',
+  },
+  {
+    value: 'Panda',
+    image: '/assets/characters/판다.svg',
+  },
+];
+
+export { characters };
