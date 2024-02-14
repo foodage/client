2861f1871cf4ffe17437f8c7402d944fc59f4b1e
Subin Lee <ewaterbinn@gmail.com>
2024-02-14 16:49:25 +0900

    feat: 캐릭터 클릭시 이벤트 작성

--- src/containers/Signup/Profile/Profile.tsx
+++ src/containers/Signup/Profile/Profile.tsx
@@ -11,8 +11,13 @@ import styles from './Profile.module.scss';
 
 const cx = classNames.bind(styles);
 
+const info =
+  '푸디지에서 사용할 닉네임과 프로필을 선택해주세요. \n 닉네임은 최대 10자까지 입력이 가능해요!';
+
 export const Profile = () => {
   const [nickname, setNickname] = useState<string>('');
+  const [image, setImage] = useState<string>(characters[0].image);
+
   const nicknameRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -24,8 +29,10 @@ export const Profile = () => {
     nicknameRef.current?.focus();
   };
 
-  const info =
-    '푸디지에서 사용할 닉네임과 프로필을 선택해주세요. \n 닉네임은 최대 10자까지 입력이 가능해요!';
+  const handleImage = (e: React.MouseEvent<HTMLDivElement>) => {
+    const imageUrl = e.currentTarget.querySelector('img')?.getAttribute('src');
+    setImage(imageUrl!);
+  };
 
   return (
     <main className={cx('profile-wrap', 'container')}>
@@ -33,7 +40,7 @@ export const Profile = () => {
         <h1 className={cx('title')}>프로필 만들기</h1>
         {/* 프로필 이미지 설정  */}
         <div className={cx('profile-image')}>
-          <Pizon className={cx('profile')} />
+          <img alt="프로필 이미지" className={cx('profile')} src={image} />
         </div>
 
         <div className={cx('nickname-wrap')}>
@@ -46,7 +53,9 @@ export const Profile = () => {
               value={nickname}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNickname(e.target.value)}
             />
-            <IconX className={cx('input-delete-btn')} onClick={handleDeleteNickname} />
+            <div className={cx('icon-wrap')} onClick={handleDeleteNickname}>
+              <IconX />
+            </div>
           </div>
           <div className={cx('desc-wrap')}>
             <p className={cx('desc')} dangerouslySetInnerHTML={{ __html: info }}></p>
@@ -58,7 +67,12 @@ export const Profile = () => {
         <h2 className={cx('title')}>프로필 캐릭터</h2>
         <div className={cx('characters')}>
           {characters.map((profile, index) => (
-            <ProfileImage key={index} name={profile.value} url={profile.image} />
+            <ProfileImage
+              key={index}
+              name={profile.value}
+              url={profile.image}
+              onClick={handleImage}
+            />
           ))}
         </div>
         <Button styleType={'primary'}>가입하기</Button>
@@ -67,9 +81,17 @@ export const Profile = () => {
   );
 };
 
-const ProfileImage = ({ url, name }: { url: string; name: string }) => {
+const ProfileImage = ({
+  url,
+  name,
+  onClick,
+}: {
+  url: string;
+  name: string;
+  onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+}) => {
   return (
-    <div className={cx('image-wrap')}>
+    <div className={cx('image-wrap')} onClick={(e) => onClick(e)}>
       <img alt={name} className={cx('image')} src={url} />
     </div>
   );
