317835c77edd7b02a5b84f5325114f637c665268
Subin Lee <ewaterbinn@gmail.com>
2024-02-26 23:43:50 +0900

    feat: 식사시간 테스트

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,3 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export const Main = () => {
-  return <div>Main</div>;
+  const [timeText, setTimeText] = useState('');
+
+  // 21:00 - 04:59 야식
+  //5:00 - 11:59 아침
+  //12:00 - 16:59 점심
+  //17:00 - 20:59 저녁
+
+  useEffect(() => {
+    const date = new Date();
+    const hours = date.getHours();
+
+    if (hours >= 21 || hours < 5) {
+      setTimeText('야식');
+    } else if (hours >= 5 && hours < 12) {
+      setTimeText('아침');
+    } else if (hours >= 12 && hours < 17) {
+      setTimeText('점심');
+    } else if (hours >= 17 && hours < 21) {
+      setTimeText('저녁');
+    }
+  }, []);
+
+  return <div>오늘 {timeText} 뭐예요?</div>;
 };
