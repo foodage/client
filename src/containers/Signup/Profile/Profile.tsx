912c748b58be15e023fdf7f50561c3b2ac0d7e40
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 00:48:45 +0900

    feat: page 에 작성한 내용 회원가입 컨테이터로 이동, 쿠키 라이브러리를 통해 사용, 회원가입 폼데이터 임의 작성, 회원가입 로직 작성

--- src/containers/Signup/Profile/Profile.tsx
+++ src/containers/Signup/Profile/Profile.tsx
@@ -1,9 +1,14 @@
 'use client';
+
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
 import { useEffect, useRef, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 import IconX from '/public/assets/icon-x.svg';
 import { Button } from '@/components';
+import { SignupCredentials } from '@/types';
 
 import { characters } from './content';
 import styles from './Profile.module.scss';
@@ -14,6 +19,9 @@ const info =
   '푸디지에서 사용할 닉네임과 프로필을 선택해주세요. \n 닉네임은 최대 10자까지 입력이 가능해요!';
 
 export const Profile = () => {
+  const router = useRouter();
+  const [cookies] = useCookies();
+
   const [nickname, setNickname] = useState<string>('');
   const [character, setCharacter] = useState({
     image: characters[2].image2,
@@ -40,12 +48,34 @@ export const Profile = () => {
     });
   };
 
-  const handleSubmit = () => {
-    const formData = {
-      nickname,
+  const handleSubmit = async () => {
+    const accessToken = cookies['Oauth-Access-Token'];
+    const serverToken = cookies['Oauth-Server-Token'];
+
+    if (!accessToken || !serverToken) {
+      return;
+    }
+    const formData: SignupCredentials = {
+      oauthServerType: 'kakao',
+      accessToken: accessToken,
+      accountEmail: '',
+      profileImage: '',
+      nickname: nickname,
       character: character.character,
     };
-    console.log(formData);
+    try {
+      const res = await signIn('credentials', {
+        ...formData,
+        redirect: false,
+      });
+      if (res?.ok) {
+        router.push('/');
+        //✅ Todo: 로그인 or 가입 후 쿠키에 저장할 값 정하기
+        //- 최근 로그인한 oauth 타입, nickname
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
