e5029af48f5ae12cd7124dbddbde27ef12ac3167
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 00:45:57 +0900

    fix: import 루트 간소화, metaData 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@ import '@/styles/globals.scss';
 import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '@/lib/auth';
+import { authOptions } from '@/lib';
 import { Providers } from '@/utils';
 
 import AuthScript from './scripts';
+
 export const metadata: Metadata = {
-  title: '푸디지 | Meal Diary',
-  description: 'Meal Diary',
+  title: '푸디지',
+  description: '나만의 음식 발자취',
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
