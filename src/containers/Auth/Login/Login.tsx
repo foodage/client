50d648f3ebd36370f3f91fb480bdb3879e9a6218
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 00:49:22 +0900

    fix: import 간소화, 컴포넌트 props 대괄호 표기

--- src/containers/Auth/Login/Login.tsx
+++ src/containers/Auth/Login/Login.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames/bind';
 import { useRef } from 'react';
 
 import { IconButton } from '@/components';
-import useLogin from '@/hooks/useLogin';
+import { useLogin } from '@/hooks';
 
 import styles from './Login.module.scss';
 
@@ -22,23 +22,24 @@ export const Login = () => {
       </div>
       <div className={cx('btn-wrap')}>
         <IconButton
-          classNames="icon-button"
+          classNames={'icon-button'}
           colorType={'kakao'}
-          iconUrl="/assets/icon-kakao.svg"
-          text="카카오로 시작하기"
+          iconUrl={'/assets/icon-kakao.svg'}
+          text={'카카오로 시작하기'}
           onClick={kakaoLogin}
         />
+
         <IconButton
-          classNames="icon-button"
-          iconUrl="/assets/icon-naver.svg"
+          classNames={'icon-button'}
+          iconUrl={'/assets/icon-naver.svg'}
           ref={naverRef}
-          text="네이버로 시작하기"
+          text={'네이버로 시작하기'}
           onClick={() => naverLogin(naverRef)}
         />
         <IconButton
-          classNames="icon-button"
-          iconUrl="/assets/icon-google.svg"
-          text="구글로 시작하기"
+          classNames={'icon-button'}
+          iconUrl={'/assets/icon-google.svg'}
+          text={'구글로 시작하기'}
           onClick={googleLogin}
         />
       </div>
