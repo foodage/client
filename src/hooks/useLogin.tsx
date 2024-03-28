c21e1d6b5fa67d0026680836ee334f68b32a9bca
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 16:57:41 +0900

    fix: 네이버 로그인 클릭시 오류 수정

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -5,17 +5,20 @@ export const useLogin = () => {
     });
   };
 
-  const naverLogin = (naverRef: React.RefObject<HTMLButtonElement>) => {
-    const loginButton = naverRef.current!.previousSibling!.firstChild as HTMLAnchorElement;
+  const naverLogin = (ref: React.RefObject<HTMLButtonElement>) => {
+    if (!ref.current || !ref.current.children) {
+      return;
+    }
+    const loginButton = ref.current.children[0] as HTMLImageElement;
     loginButton.click();
   };
 
   const googleLogin = () => {
     const client = google.accounts.oauth2.initCodeClient({
       client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
       ux_mode: 'redirect',
       scope: 'email profile',
-      redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
     });
     client.requestCode();
   };
