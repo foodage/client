5a6db1bcfb04572d4c42fce411c0ae89b9283b66
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 16:58:34 +0900

    refactor: head 내에 script 작성, layout 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,18 @@
 import '@/styles/globals.scss';
 
-import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 
 import { authOptions } from '@/lib';
 import { Providers } from '@/utils';
 
-import AuthScript from './scripts';
-
-export const metadata: Metadata = {
-  title: '푸디지',
-  description: '나만의 음식 발자취',
-};
+import Head from './head';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
   return (
     <html lang="ko">
-      <head>
-        <AuthScript />
-      </head>
+      <Head />
       <Providers session={session}>
         <body>{children}</body>
       </Providers>
