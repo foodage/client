15b6929ecbf3eb22761823594b92d0aa573a8d0b
Subin Lee <ewaterbinn@gmail.com>
2024-04-29 19:38:09 +0900

    style: splash 구조, 에니메이션 수정

--- src/containers/Auth/Login/Login.tsx
+++ src/containers/Auth/Login/Login.tsx
@@ -21,47 +21,49 @@ export const Login = () => {
 
   return (
     <main className={cx('container', 'login-container')}>
-      <img alt="foodage" className={cx('background')} src="/assets/images/login-background.svg" />
+      <div className={cx('splash-wrap')}>
+        <img alt="foodage" className={cx('background')} src="/assets/images/login-background.svg" />
 
-      <div className={cx('login-section')}>
-        <div className={cx('logo-wrap')}>
-          <h1 className={cx('logo')}>FOODAGE</h1>
-          <span className={cx('introduce')}>나만의 맛집 발자취, 푸디지</span>
+        <div className={cx('login-section')}>
+          <div className={cx('logo-wrap')}>
+            <h1 className={cx('logo')}>FOODAGE</h1>
+            <span className={cx('introduce')}>나만의 맛집 발자취, 푸디지</span>
+          </div>
+        </div>
+      </div>
+
+      <div className={cx('button-section')}>
+        <div className={cx('auth-btn-wrap')}>
+          <IconButton
+            classNames={'icon-button'}
+            colorType={'kakao'}
+            iconUrl={'/assets/icons/icon-kakao.svg'}
+            text={'카카오로 시작하기'}
+            onClick={kakaoLogin}
+          />
+
+          <button className={cx('hidden')} id={'naverIdLogin'} ref={naverRef} />
+          <IconButton
+            classNames={'icon-button'}
+            iconUrl={'/assets/icons/icon-naver.svg'}
+            text={'네이버로 시작하기'}
+            onClick={() => naverLogin(naverRef)}
+          />
+
+          <IconButton
+            classNames={'icon-button'}
+            iconUrl={'/assets/icons/icon-google.svg'}
+            text={'구글로 시작하기'}
+            onClick={googleLogin}
+          />
         </div>
 
-        <div className={cx('button-section')}>
-          <div className={cx('auth-btn-wrap')}>
-            <IconButton
-              classNames={'icon-button'}
-              colorType={'kakao'}
-              iconUrl={'/assets/icons/icon-kakao.svg'}
-              text={'카카오로 시작하기'}
-              onClick={kakaoLogin}
-            />
-
-            <button className={cx('hidden')} id={'naverIdLogin'} ref={naverRef} />
-            <IconButton
-              classNames={'icon-button'}
-              iconUrl={'/assets/icons/icon-naver.svg'}
-              text={'네이버로 시작하기'}
-              onClick={() => naverLogin(naverRef)}
-            />
-
-            <IconButton
-              classNames={'icon-button'}
-              iconUrl={'/assets/icons/icon-google.svg'}
-              text={'구글로 시작하기'}
-              onClick={googleLogin}
-            />
+        <div className={cx('inquiry-wrap')}>
+          <div className={cx('left')} onClick={() => router.push('/inquiry')}>
+            <span className={cx('text-button')}>문의하기</span>
+            <Arrow className={cx('arrow-icon')} />
           </div>
-
-          <div className={cx('inquiry-wrap')}>
-            <div className={cx('left')} onClick={() => router.push('/inquiry')}>
-              <span className={cx('text-button')}>문의하기</span>
-              <Arrow className={cx('arrow-icon')} />
-            </div>
-            <span className={cx('version')}>{FOODAGE_VERSION}</span>
-          </div>
+          <span className={cx('version')}>{FOODAGE_VERSION}</span>
         </div>
       </div>
     </main>
